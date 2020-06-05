50eb67609956318d82ac77149d2dcae95cd174af
rishav <rishav.21m@gmail.com>
2020-06-05 15:20:15 +0530

    added client

---
+++ build/precache-manifest.d062672b2ad49fa06743017b9c550c0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "727a1c976cb22de2792ed54a0c919773",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c522817cb0ad33a8197",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "59dcf0481d51f3595963",
+    "url": "/static/js/2.95510dbb.chunk.js"
+  },
+  {
+    "revision": "0ac79d53dda6ffa0c6325c90109848cb",
+    "url": "/static/js/2.95510dbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c522817cb0ad33a8197",
+    "url": "/static/js/main.f4a13215.chunk.js"
+  },
+  {
+    "revision": "544ef871788469cfecd7",
+    "url": "/static/js/runtime-main.f801d0fc.js"
+  }
+]);
